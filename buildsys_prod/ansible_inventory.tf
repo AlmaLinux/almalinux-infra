d35546934c97b6db9ab38c672c53eded815f58dd
Eugene Zamriy <eugene@zamriy.info>
2021-05-20 14:42:34 +0300

    Adds 2 new ARM nodes to generated Ansible inventory file

--- buildsys_prod/ansible_inventory.tf
+++ buildsys_prod/ansible_inventory.tf
@@ -4,20 +4,27 @@
 
 resource "local_file" "ansible_inventory" {
   filename = "inventory-prod.yml"
+  file_permission = "644"
   content  = yamlencode({
     all = {
       hosts = {
         (metal_device.arm64-builder01.network.0.address) = {}
+        (metal_device.arm64-builder02.network.0.address) = {}
+        (metal_device.arm64-builder03.network.0.address) = {}
       }
       children = {
         arm_hosts = {
           hosts = {
             (metal_device.arm64-builder01.network.0.address) = {}
+            (metal_device.arm64-builder02.network.0.address) = {}
+            (metal_device.arm64-builder03.network.0.address) = {}
           }
         }
         build_nodes = {
           hosts = {
             (metal_device.arm64-builder01.network.0.address) = {}
+            (metal_device.arm64-builder02.network.0.address) = {}
+            (metal_device.arm64-builder03.network.0.address) = {}
           }
         }
       }
